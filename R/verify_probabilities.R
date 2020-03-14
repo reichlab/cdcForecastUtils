a60ad3c4cfb7931392091c8bb51162798f86305b
Nutcha <ahctun_woon@Nutchas-MacBook-Pro.local>
2020-03-14 15:12:19 -0400

    verification update

---
+++ R/verify_probabilities.R
@@ -0,0 +1,83 @@
+#' Verify the entry probabilities
+#'
+#' @param entry An entry data.frame
+#' @import dplyr
+#' @return Invisibly returns \code{TRUE} or a descriptive error message
+#' @export
+#' @keywords internal
+#' @seealso \code{\link{verify_entry}}
+#' @examples 
+#' verify_probabilities(full_entry_new)
+#' 
+verify_probabilities <- function(entry) {
+  
+  names(entry) <- tolower(names(entry))
+  
+  probabilities <- entry %>%
+    dplyr::filter(type == "bin",target!="Below baseline for 3 weeks") %>%
+    dplyr::group_by(location, target) %>%
+    dplyr::summarize(
+              miss     = any(is.na(value)),
+              sum      = sum(value, na.rm = TRUE),
+              negative = any(!is.na(value) & value < 0))
+  
+  probabilities_binary <- entry %>%
+    dplyr::filter(type == "bin",target=="Below baseline for 3 weeks") %>%
+    dplyr::group_by(location) %>%
+    dplyr::summarize(
+      miss     = any(is.na(value)),
+      more_than_one = any(!is.na(value) & value > 1),
+      negative = any(!is.na(value) & value < 0))
+  
+  errors <- character()
+  # Report message for missing probabilities
+  # fix for intentional missing for targets not forecasted
+  if (any(probabilities$miss)) {
+    tmp <- probabilities %>%
+      dplyr::filter(miss)
+    errors <- c(errors, paste0("ERROR: Missing probabilities detected in ",
+                                 paste(tmp$location, tmp$target), ".\n"))
+  }
+  if (any(probabilities_binary$miss)) {
+    tmp <- probabilities_binary %>%
+      dplyr::filter(miss)
+    errors <- c(errors, paste0("ERROR: Missing probabilities detected in ",
+                               paste(tmp$location, tmp$target), ".\n"))
+  }
+  # Report message for negative probabilities
+  if (any(probabilities$negative)) {
+    tmp <- probabilities %>%
+      dplyr::filter(negative)
+    
+    errors <- c(errors, paste0("ERROR: Negative probabilities detected in ",
+                               paste(tmp$location, tmp$target), ".\n"))
+  }
+  if (any(probabilities_binary$negative)) {
+    tmp <- probabilities_binary %>%
+      dplyr::filter(negative)
+    
+    errors <- c(errors, paste0("ERROR: Negative probabilities detected in ",
+                               paste(tmp$location, tmp$target), ".\n"))
+  }
+  # Report message for sum of target probabilities outside of 0.9 and 1.1
+  if (any(probabilities$sum < 0.9 | probabilities$sum > 1.1)) {
+    tmp <- probabilities %>%
+      dplyr::filter(sum<0.9 | sum>1.1)
+    
+    errors <- c(errors, paste0("ERROR: In ", tmp$location, "-", tmp$target, ", probabilities sum to ",
+                               tmp$sum, ". \n"))
+  }
+  if (any(probabilities_binary$more_than_one)) {
+    tmp <- probabilities_binary %>%
+      dplyr::filter(more_than_one)
+    
+    errors <- c(errors, paste0("ERROR: Higher than 1 binary probabilities detected in ",
+                               paste(tmp$location, tmp$target), ".\n"))
+  }
+  #Output probability related errors
+  if (length(errors) != 0) {
+    stop(errors)
+  }
+
+  return(invisible(TRUE))
+}
---
+++ R/verify_types.R
@@ -0,0 +1,41 @@
+#' Verify types are correct
+#'
+#' The necessary types depend on the type, so this will verify types are correct
+#' for all types
+#'
+#' @param entry An entry data.frame
+#' @param challenge one of "ilinet" or "state_ili", indicating which
+#'   challenge the submission is for
+#' @return Invisibly returns TRUE if successful
+#' @export
+#' @keywords internal
+#' @seealso \code{\link{verify_entry}}
+#' @examples
+#' verify_types(full_entry_new)
+verify_types <- function(entry, challenge = "ilinet") {
+  
+  if (!(challenge %in% c("ilinet", "state_ili"))) {
+    stop("challenge must be one of ilinet, hospital, or state_ili")
+  }
+  
+  names(entry) <- tolower(names(entry))
+  
+  # ILINet challenge
+  if (challenge == "ilinet") {
+    valid_types <- unique(cdcForecastUtils::full_entry_new$type)
+  } else if (challenge == "state_ili") {
+    valid_types <- unique(cdcForecastUtils::full_entry_state_new$type)
+  } 
+  entry_types <- unique(entry$type)
+  
+  missing_types <- setdiff(valid_types, entry_types)
+  extra_types   <- setdiff(entry_types, valid_types)
+  
+  if (length(missing_types)>0)
+    stop("Missing these types: ", paste(missing_types))
+  
+  if (length(extra_types)>0 && extra_types != "point")
+    warning("These extra types are ignored: ", paste(extra_types))
+  
+  return(invisible(TRUE))
+}
