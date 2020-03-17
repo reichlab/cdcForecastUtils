f239cb3ef7d39f06edeb7b0fc0d17126e83931e6
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2020-03-17 13:05:32 -0400

    minor changes

--- R/verify_locations.R
+++ R/verify_locations.R
@@ -39,14 +39,15 @@ verify_locations <- function(entry, challenge = "ilinet") {
   extra_locations   <- setdiff(entry_locations, valid_locations)
   possible_locations <- setdiff(valid_locations, entry_locations)
   if (length(intersect(entry_locations, valid_locations))==0){
-    stop("Missing all valid locations. Wrong challenge?: ", paste(extra_locations))
+    stop("Missing all valid locations. Wrong challenge?: ", paste(extra_locations,sep=""))
   }
   if (length(extra_locations)>0){ 
-    warning("These extra locations are ignored. Please check for possible spelling errors: ", paste(extra_locations))
+    warning("These extra locations are ignored. Please check for possible spelling errors: ", 
+            paste(extra_locations,sep=""))
   }
   if (length(possible_locations)>0)
     # message("Consider forecasting for these locations: ", paste(possible_locations))
-    message("These locations have no forecast: ", paste(possible_locations))
+    message("These locations have no forecast: ", paste(possible_locations,sep=""))
   
   return(invisible(TRUE))
 }
