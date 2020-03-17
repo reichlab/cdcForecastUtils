9295c550ef027a2abbce48d26caa3fa4ce6e80cc
graham gibson <gcgibson@umass.edu>
2020-03-17 14:46:24 -0600

    more unit tests

---
+++ tests/testthat/test-generate_csv_from_submission_df.R
@@ -0,0 +1,7 @@
+context("generate_csv_from_submission_df")
+
+test_that("Read and Generate CSV ", {
+  submission_df <- read_entry("extdata/example_submission_df_for_unit_test.csv")
+  # i don't really know what to put here lol, will come back to this  
+
+})
