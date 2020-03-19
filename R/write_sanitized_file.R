ed0f34f6ae527240c295d193ef7e580ec3547cfc
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2020-03-19 09:17:18 -0400

    edit write sanitized file

--- R/write_sanitized_file.R
+++ R/write_sanitized_file.R
@@ -1,13 +1,13 @@
 #' Write a sanitized file
 #'
-#' This function reads in the csv file and arranges it for consistency.
+#' This function reads in the csv file and arranges it for consistency and overwrite.
 #'
 #' @param file A csv file path
+#' @param challenge Either "ilinet" or "state_ili", indicating challenge the submission is for.
 #' @return A reformatted csv file
 #' @import dplyr
 #' @export
 write_sanitized_file <- function(file, challenge = "ilinet") {
-  
   entry <- cdcForecastUtils::read_entry(file)
   model_name<-basename(file)
   if(cdcForecastUtils::verify_entry(entry, challenge, check_week = F)){
