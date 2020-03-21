69777ec7c94db2bb7ddf947916411cc4be6bfedb
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2020-03-20 23:47:50 -0400

    fix travis build issue

--- R/get_targets_regions.R
+++ R/get_targets_regions.R
@@ -16,8 +16,10 @@ get_targets_regions<- function(sanitized_file) {
     dplyr::ungroup() %>%
     unique()
   model_name<-substr(basename(sanitized_file),11,nchar(basename(sanitized_file))-4)
+  formatted_filename <- ifelse(grepl("2020-EW[0-9]{2}",sanitized_file),
+                               gsub("2020-EW",replacement="2020-ew",sanitized_file),sanitized_file)
   forecast_week<-as.numeric(gsub("ew", "", 
-                                 regmatches(sanitized_file, regexpr("(?:ew)[0-9]{2}", sanitized_file))))
+                                 regmatches(formatted_filename, regexpr("(?:ew)[0-9]{2}", formatted_filename))))
   date_file <- cdcForecastUtils::covid_19_forecast_dates
   set$model_name <- model_name
   set$forecasts_due <- date_file$forecasts_due[
